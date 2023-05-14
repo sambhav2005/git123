8cc99b84bc3c8ccd77198f4983cea9f181057a60
= <=>
2023-05-14 09:46:41 +0530

    added multiply.js

--- multiply.js
+++ multiply.js
@@ -1,3 +1,5 @@
-const numbers = [1,2,3,4];
-const mod = numbers.reduce((accumulator,currentvalue) => accumulator + currentvalue , 0);
-console.log(mod);
+let animals = {
+    name :"cow";
+    legs:4;
+}
+console.log(animals);
